3ee5073d858c7efe08fd15753fa799e9eba7b084
Sberrich <darksamir2012@gmail.com>
2021-04-14 15:48:19 +0000

    check

--- src/Pages/Library.js
+++ src/Pages/Library.js
@@ -489,7 +489,7 @@ export default function Library() {
           <input
             className='search'
             type='search'
-            placeholder= {t("Search")}
+            placeholder={t("Search")}
             value={searchTerm}
             onChange={handleOnChange}
           />
@@ -538,7 +538,7 @@ export default function Library() {
             <div>
               <FormControlMdf>
                 <InputLabel id='demo-simple-select-helper-label'>
-                {t("gender")}
+                  {t("gender")}
                 </InputLabel>
                 <Select
                   labelId='demo-simple-select-helper-label'
