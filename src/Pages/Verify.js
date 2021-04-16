7b38b8829edc6c227df2b3db43a251b96090836f
Loubna Soulimani <lsoulima@e2r10p11.1337.ma>
2021-04-16 11:48:55 +0000

    updates

--- src/Pages/Verify.js
+++ src/Pages/Verify.js
@@ -108,7 +108,7 @@ export default function Verify() {
                 color: "#fff",
               }}
             >
-              {t("Failed to activate your account !")}
+              {t(state.message)}
             </Typography>
             <form className="form">
               <Button
